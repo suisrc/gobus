fc7aaf0eac43b77a006eba20964a027922daeff2
suisrc <suisrc@outlook.com>
2022-01-24 14:45:38 +0800

    del * config all method

--- sub_tag.go
+++ sub_tag.go
@@ -31,26 +31,22 @@ func SubscribeTag(bus Bus, data interface{}, verify bool, tag string) (func(), e
 		if tag == "" {
 			continue
 		}
-		// *,com.suisrc.", "*"=>必须使用配置文件订阅
 		// Exe00=com.suisrc.topicA#groupA>>com.suisrc.topicB，不读取配置
 		// Exe11=, => Exe11=$[method]
 		// Exe21=$topics.exe11，读取配置文件
 		// Exe31=$topics.exe11?com.suisrc.topicA#groupA
 		// Exe41=$,com.suisrc.topicA#groupA
 
-		all := tag[0] == '*'
 		cfs := make(map[string]string)
-		if !all {
-			for _, v := range strings.Split(tag, ",") {
-				if v == "" {
-					continue
-				}
-				v2 := strings.SplitN(v, "=", 2)
-				if len(v2) == 1 || v2[1] == "" {
-					cfs[v2[0]] = "$"
-				} else {
-					cfs[v2[0]] = v2[1]
-				}
+		for _, v := range strings.Split(tag, ",") {
+			if v == "" {
+				continue
+			}
+			v2 := strings.SplitN(v, "=", 2)
+			if len(v2) == 1 || v2[1] == "" {
+				cfs[v2[0]] = "$"
+			} else {
+				cfs[v2[0]] = v2[1]
 			}
 		}
 		tfv := val.Field(k)
@@ -66,14 +62,7 @@ func SubscribeTag(bus Bus, data interface{}, verify bool, tag string) (func(), e
 				continue // 方法不存在跳过
 			}
 			topic := ""
-			if all { // 全部方法都需要订阅
-				tic := strings.ToLower(name)
-				if len(tag) > 1 { // 默认主题 // $method#method ?tag-method
-					topic = fmt.Sprintf("$%s#%s%s%s", tic, tic, tag[1:], tic)
-				} else {
-					topic = fmt.Sprintf("$%s#%s", tic, tic)
-				}
-			} else if conf == "" || conf[0] != '$' {
+			if conf == "" || conf[0] != '$' {
 				topic = conf
 			} else if cs := strings.SplitN(conf[1:], "?", 2); cs[0] != "" {
 				topic = conf
